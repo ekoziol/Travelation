c9baa619713f9695726c95e0fcdd6d45f9a2a077
Eric Koziol <e.trip.k@gmail.com>
2013-07-16 15:21:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Travelation::Application.config.secret_key_base = '66a8a4e385e5d645953a5a9f6b3b9fd426f0e4d47be3a7a651d384826420746a9da712de0b2f8f0f034c1c7c9ea0bd21e855afdfbbd2597eb0846b0a42c6b38e'
